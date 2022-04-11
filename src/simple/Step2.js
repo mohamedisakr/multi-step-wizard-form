9afb49fa2ed1d6e77feb50a73d5002f21c8a7fef
mohamedisakr <mohamedisakr@gmail.com>
2022-04-11 13:39:04 +0200

    implementing new step 2

--- src/simple/Step2.js
+++ src/simple/Step2.js
@@ -2,20 +2,20 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { chooseLastName } from "../rootSlice";
+import { chooseEmail } from "../rootSlice";
 
 export const Step2 = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const crust = useSelector((state) => state.crust);
+  const email = useSelector((state) => state.email);
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: "onBlur", defaultValues: { crust } });
+  } = useForm({ mode: "onBlur", defaultValues: { email } });
 
   const onSubmit = (data) => {
-    dispatch(chooseLastName(data.crust));
+    dispatch(chooseEmail(data.email));
     navigate("/step3");
   };
 
@@ -25,7 +25,7 @@ export const Step2 = () => {
         type="email"
         id="email"
         name="email"
-        {...register("email")}
+        {...register("email", { required: "Email is required" })}
         className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
         placeholder="Email address *"
       />
